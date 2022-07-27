e431f10cccd007c4b03f513fb76351808a685172
Splqshes <ponleousk@gmail.com>
2022-07-27 20:52:47 +0700

    Attempted to build the page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-const collapsibles = document.querySelectorAll(".collapsible__toggler")
+// collapsible
+const collapsibles = document.querySelectorAll(".collapsible__toggler");
 
-
-collapsibles.forEach(item => item.addEventListener("click", function() {
+collapsibles.forEach(item => item.addEventListener("click", 
+function() {
     item.classList.toggle("collapsible--expanded");
-    })
-);
+}));
+
+//hover animation (mobile)
+const mainPage = document.querySelectorAll(".mainpage");
+const hoverEl = document.querySelectorAll(".tap")
+
+function toggleClass(){
+    hoverEl.forEach(item => item.classList.toggle("tap--active"))
+};
+
+mainPage.forEach(item => item.addEventListener("touchstart", toggleClass));
+//mainPage.forEach(item => item.addEventListener("click", toggleClass));
